5c01b65b8eccc65b69fc814526236575fa4fc388
cooper-ece160 <j_astrauckas@yahoo.com>
2017-09-20 18:52:09 -0400

    Two ways to print number strings

--- strcmp-test.c
+++ strcmp-test.c
@@ -1,4 +1,36 @@
-int main()
+#include<stdio.h>
+#include<string.h>
+
+int main(int argc, char *argv[])
 {
+	/*
+	for (int i = 1; i < argc; ++i) {
+		char c = argv[i][0];
+		switch(c) {
+			case('1'):
+			  printf("one\n");
+			  break;
+			case('2'):
+			  printf("two\n");
+			  break;
+			case('3'):
+			  printf("three\n");
+			  break;
+			default:
+			  printf("default\n");
+		}
+	}
+	*/
+	for (int i = 1; i < argc; ++i) {
+		if (strcmp(argv[i], "1") == 0) {
+			printf("one\n");
+		} else if (strcmp(argv[i], "2") == 0) {
+			printf("two\n");
+		} else if (strcmp(argv[i], "3") == 0) {
+			printf("three\n");
+		} else {
+			printf("default\n");
+		}
+	}
 	return 0;
 }
